4d164ddd6b3a874a887c363015c95a1c0771e421
maksim <maksimsar1@gmail.com>
2020-12-19 14:49:34 +0400

    last commit

---
+++ miles.java
@@ -0,0 +1,8 @@
+public class miles {
+    public static void main(String[] args) {
+        int price = 2000;
+        int charge = 20;
+        int bonus = price / charge;
+        System.out.println(bonus);
+    }
+}
